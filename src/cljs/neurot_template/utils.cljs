134a489ef15c20e4b07b6ccf52a046d66dfe4f13
neurot <neurot@resisting-mind.com>
2017-06-06 15:33:39 +0200

    styling

---
+++ src/cljs/neurot_template/utils.cljs
@@ -0,0 +1,3 @@
+(ns neurot-template.utils)
+
+(def lorem "Und es war ihnen wie eine Bestätigung ihrer neuen Träume und guten Absichten, als am Ziele ihrer Fahrt die Tochter als erste sich erhob und ihren jungen Körper dehnte. »Es ist ein eigentümlicher Apparat«, sagte der Offizier zu dem Forschungsreisenden und überblickte mit einem gewissermaßen bewundernden Blick den ihm doch wohlbekannten Apparat.")
--- src/cljs/neurot_template/views.cljs
+++ src/cljs/neurot_template/views.cljs
@@ -1,5 +1,6 @@
 (ns neurot-template.views
-    (:require [re-frame.core :refer [subscribe dispatch]]))
+  (:require [re-frame.core :refer [subscribe dispatch]]
+            [neurot-template.utils :as util]))
 
 ;; main panel
 
@@ -9,8 +10,9 @@
         avg       (subscribe [:average])]
     (fn []
       [:div
-       [:div.em33
-        [:h1 @name "."]
+       [:div.em33.bgpink
+        [:h1.bgb @name]
+        [:p.bgw util/lorem]
         [:input {:type      :number
                  :on-change #(dispatch [:number-changed (-> % .-target .-value)])
                  :value     @my-number}]
