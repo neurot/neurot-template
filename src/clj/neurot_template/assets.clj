1f50c1facf054e83098bb9c84c67f63d94b3e393
neurot <neurot@resisting-mind.com>
2017-06-21 01:22:32 +0200

    add data resampling

--- src/clj/neurot_template/assets.clj
+++ src/clj/neurot_template/assets.clj
@@ -8,41 +8,25 @@
             [clojure.string :as str]
             [neurot-template.talib :refer [tx ta ta-info price-holder open high low close volume]]))
 
-; Redis
+;; Redis
 
 (def server1-conn {:pool {} :spec {:host "127.0.0.1" :port 6379}})
 (defmacro wcar* [& body] `(car/wcar server1-conn ~@body))
 
 (def time-formatter (f/formatter "yyyyMMdd HHmmss"))
 
-; Data Creation
+;; Data Input
 
 (defn format-raw-data [dta]
   (mapv (fn [dta] [(+ (tc/to-long (f/parse time-formatter (first dta)))
-                     18000000) ; est -> utc compensation
-                  (read-string (nth dta 1))
-                  (read-string (nth dta 2))
-                  (read-string (nth dta 3))
-                  (read-string (nth dta 4))])
-       (map #(split % #";") ;split each line by ;
+                      18000000) ; est -> utc compensation
+                   (read-string (nth dta 1))
+                   (read-string (nth dta 2))
+                   (read-string (nth dta 3))
+                   (read-string (nth dta 4))])
+        (map #(split % #";") ;split each line by ;
             ;; (reverse)
-            (split dta #"\n"))))
-
-
-(defn get-talib [asset-data ta-func]
-  (let [talib-data (reverse (seq (first (tx ta-func asset-data))))]
-    (mapv #(vector (first %1) %2) asset-data talib-data)))
-
-(defn resample-data [asset]
-  (str/split asset #"/"))
-
-(defn get-asset [asset ta-function]
-  (let [asset-data (resample-data (wcar* (car/get asset)))]
-    {:asset {:name asset
-             :data asset-data}
-     :talib {:info (ta-info ta-function)
-             :data (get-talib asset-data ta-function)}}
-    ))
+             (split dta #"\n"))))
 
 (defn filename-to-key [filename]
   (let [name-array (-> (second (re-matches #"(.+?)(\.[^.]*$|$)" filename))
@@ -60,24 +44,54 @@
                         (format-raw-data (slurp (str dir "/" %))))
        (get-csv-files dir)))
 
-
-;; (write-to-redis "test" (format-raw-data (slurp "data/test/test.csv")))
-
-;; mass import
-;; (import-raw-csv "data")
+; mass import
+(defn import-data-dir! [] (import-raw-csv "data"))
+; (write-to-redis "test" (format-raw-data (slurp "data/test/test.csv")))
 
 
+;; Data Output
 
-;; (def DEMO-data (wcar* (car/get "test")))
+(defn get-talib [asset-data ta-func]
+  (let [talib-data (reverse (seq (first (tx ta-func asset-data))))]
+    (mapv #(vector (first %1) %2) asset-data talib-data)))
 
-;; (def DEMO (price-holder (wcar* (car/get "test"))))
+;; (def test-dta (take 5 (wcar* (car/get "TEST/M1/2017"))))
 
-;; (filter #(> 0 %) (seq (first (ta "rsi" [(close DEMO)] 20))))
 
-;; (def DEMO-l (price-holder "test-l"))
+(defn recalc-ohlc [dta]
+  [(first (last dta))
+   (second (first dta))
+   (reduce (fn [val dta] (let [[_ _ h l] dta] (max val h))) 0.0 dta)
+   (reduce (fn [val dta] (let [[_ _ h l] dta] (min val l))) (nth (first dta) 3) dta)
+   (last (last dta))])
 
-;; (filter #(> 0 %) (seq (first (ta "cdlharami" [DEMO-l]))))
+(defn resample-data
+  ([input freq]
+   (resample-data input freq []))
+  ([input freq output]
+   (if (empty? input)
+     output
+     (recur (drop freq input) freq (conj output (recalc-ohlc (take freq input)))))))
 
-;; (ta "ADD" [(double-array [10 100 1000]) (double-array [1 2 3])])
+(defn get-data [request]
+  (let [meta (str/split request #"/")
+        asset (first meta)
+        freq (read-string (re-find #"\d+" (second meta)))
+        year (nth meta 2)
+        raw-data (wcar* (car/get (str asset "/M1/" year)))]
+    (if (= freq 1)
+      raw-data
+      (resample-data raw-data freq))))
 
-;; (wcar* (car/get "asset/test"))
+;; (first (take 1 test-dta))
+
+;; (resample-data test-dta 3 [])
+
+;; (get-data "TEST/M1/2017")
+
+(defn get-asset [asset ta-function]
+  (let [asset-data (get-data (str/upper-case asset))]
+    {:asset {:name (str/upper-case asset)
+             :data asset-data}
+     :talib {:info (ta-info ta-function)
+             :data (get-talib asset-data ta-function)}}))
---
+++ src/cljs/neurot_template/asset.cljs
@@ -0,0 +1,1 @@
+(ns neurot-template.asset)
