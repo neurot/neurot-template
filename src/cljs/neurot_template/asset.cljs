2379543b660cf2d1fccf0566e934fa4701f331e1
neurot <neurot@resisting-mind.com>
2017-06-21 15:12:27 +0200

    refactoring

--- src/cljs/neurot_template/asset.cljs
+++ src/cljs/neurot_template/asset.cljs
@@ -1,1 +1,36 @@
-(ns neurot-template.asset)
+(ns neurot-template.asset
+  (:require [re-frame.core :refer [subscribe dispatch]]
+            [reagent.core :as reagent]
+            [cljsjs.highstock]))
+
+(defn render-stock-fn [cnfg]
+  (fn [component]
+    (.stockChart js/Highcharts (reagent/dom-node component) (clj->js @cnfg))))
+
+(defn chart [cnfg]
+  (reagent/create-class
+   {:component-did-mount (render-stock-fn cnfg)
+    :component-did-update (render-stock-fn cnfg)
+    :reagent-render (fn [cnfg]
+                      @cnfg
+                      [:div])}))
+
+(defn ui []
+  (let [cnfg  (subscribe [:chart-cnfg])
+        asset (subscribe [:asset])
+        talib (subscribe [:talib])]
+    [:div
+     [:div.em33
+      [:input.input {:type        "text"
+                     :placeholder "Asset"
+                     :value       @(subscribe [:asset])
+                     :on-change   #(dispatch [:asset-change (-> % .-target .-value)])}]
+      [:input.input {:type        "text"
+                     :placeholder "Technical Analyzer"
+                     :value       @(subscribe [:talib])
+                     :on-change   #(dispatch [:talib-change (-> % .-target .-value)])}]
+      [:button.btn.btn-outline.black
+       {:on-click #(dispatch [:assets/get {:asset @asset :talib @talib}])}
+       "Load Asset"]]
+     [:div.stock.m1
+      [chart cnfg]]]))
--- src/cljs/neurot_template/views.cljs
+++ src/cljs/neurot_template/views.cljs
@@ -2,52 +2,24 @@
   (:require [re-frame.core :refer [subscribe dispatch]]
             [reagent.core :as reagent]
             [neurot-template.utils :as util]
-            [cljsjs.highstock]
+            [neurot-template.asset :as asset]
             [goog.string :as gstring]))
-
-(defn render-stock-fn [cnfg]
-  (fn [component]
-    (.stockChart js/Highcharts (reagent/dom-node component) (clj->js @cnfg))))
-
-(defn stock-ui [cnfg]
-  (reagent/create-class
-   {:component-did-mount (render-stock-fn cnfg)
-    :component-did-update (render-stock-fn cnfg)
-    :reagent-render (fn [cnfg]
-                      @cnfg
-                      [:div])}))
 
 ;; main panel
 
 (defn main-panel []
-  (let [name      (subscribe [:name])
-        connected?    (subscribe [:connected?])
-        cnfg (subscribe [:chart-cnfg])
-        asset (subscribe [:asset])
-        talib (subscribe [:talib])]
+  (let [name       (subscribe [:name])
+        connected? (subscribe [:connected?])]
     (fn []
       [:div
        ;; [:div.menu "menu"]
        ;; [:div.settings "settings"]
-       [:div.status (if @connected? [:p.status (gstring/unescapeEntities "&#9901;")]  [:p.status.red (gstring/unescapeEntities "&#9902;")])]
        ;; [:div.info "info"]
        [:div.em33
         [:h1 @name]]
        [:div.em33
         [:p.bgw-basker util/lorem-tweet]]
-       [:div.em33
 
-        [:input.input {:type "text"
-                       :placeholder "Asset"
-                       :value @(subscribe [:asset])
-                       :on-change #(dispatch [:asset-change (-> % .-target .-value)])}]
+       [asset/ui]
 
-        [:input.input {:type "text"
-                       :placeholder "Technical Analyzer"
-                       :value @(subscribe [:talib])
-                       :on-change #(dispatch [:talib-change (-> % .-target .-value)])}]
-
-        [:button.btn.btn-outline.black {:on-click #(dispatch [:assets/get {:asset @asset :talib @talib}])} "Load Asset"]]
-       [:div.stock.m1
-        [stock-ui cnfg]]
-       ])))
+       [:div.status (if @connected? [:p.status (gstring/unescapeEntities "&#9901;")]  [:p.status.red (gstring/unescapeEntities "&#9902;")])]])))
